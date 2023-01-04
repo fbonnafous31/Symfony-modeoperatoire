3916901540d04d5055e8d51400fdc722a6b2e969
François Bonnafous <francois.bonnafous@hotmail.fr>
2023-01-04 11:35:33 +0100

    Création de l'entité Etape de type 1-n

--- cuisine/src/Entity/Recette.php
+++ cuisine/src/Entity/Recette.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\RecetteRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -36,6 +38,16 @@ class Recette
      * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $Prix;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Etape::class, mappedBy="recette")
+     */
+    private $etapes;
+
+    public function __construct()
+    {
+        $this->etapes = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -86,6 +98,36 @@ class Recette
     public function setPrix(?string $Prix): self
     {
         $this->Prix = $Prix;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Etape>
+     */
+    public function getEtapes(): Collection
+    {
+        return $this->etapes;
+    }
+
+    public function addEtape(Etape $etape): self
+    {
+        if (!$this->etapes->contains($etape)) {
+            $this->etapes[] = $etape;
+            $etape->setRecette($this);
+        }
+
+        return $this;
+    }
+
+    public function removeEtape(Etape $etape): self
+    {
+        if ($this->etapes->removeElement($etape)) {
+            // set the owning side to null (unless already changed)
+            if ($etape->getRecette() === $this) {
+                $etape->setRecette(null);
+            }
+        }
 
         return $this;
     }
