f6fc0951263540a177a088eed1e769dd70c5294d
François Bonnafous <francois.bonnafous@hotmail.fr>
2023-01-04 16:54:44 +0100

    Mise en place d'un système d'authentification

--- cuisine/src/DataFixtures/AppFixtures.php
+++ cuisine/src/DataFixtures/AppFixtures.php
@@ -2,15 +2,39 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Ingredient;
+use App\Entity\User;
 use App\Entity\Ustensile;
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Ingredient;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    protected $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
+        $admin = new User;
+        $hash = $this->hasher->hashPassword($admin, "password");
+
+        $admin->setEmail("admin@mykitchen.com")
+            ->setPassword($hash)
+            ->setRoles(['ROLE_ADMIN']);
+        $manager->persist($admin);
+
+        $user = new User;
+        $hash = $this->hasher->hashPassword($user, "password");
+
+        $user->setEmail("user@mukitchen.com")
+            ->setPassword($hash);
+        $manager->persist($user);
+
         $file = fopen(__DIR__ . '/../../public/csv/Ingredients.csv', 'r');
         while (!feof($file)) {
             $ingredient = new Ingredient;
