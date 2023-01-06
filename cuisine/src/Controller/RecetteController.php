f8fb61750cd3902b206a8c0620a9b5c4b98ae3e8
François Bonnafous <francois.bonnafous@hotmail.fr>
2023-01-06 18:32:47 +0100

    Enregistrement de la recette

--- cuisine/src/Controller/RecetteController.php
+++ cuisine/src/Controller/RecetteController.php
@@ -2,22 +2,24 @@
 
 namespace App\Controller;
 
-use App\Entity\CategoriesPrix;
-use App\Entity\NiveauDifficulte;
+use App\Entity\Recette;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\FormFactoryInterface;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
 
 class RecetteController extends AbstractController
 {
     /**
      * @Route("/admin/recette/create", name="recette_create")
      */
-    public function create(FormFactoryInterface $factory)
+    public function create(FormFactoryInterface $factory, Request $request, EntityManagerInterface $em)
     {
-        $builder = $factory->createBuilder();
+        $builder = $factory->createBuilder(FormType::class, null, ['data_class' => Recette::class]);
 
         $builder
             ->add('nom', TextType::class, [
@@ -32,24 +34,37 @@ class RecetteController extends AbstractController
                     'placeholder' => 'Durée de préparation'
                 ]
             ])
-            ->add('difficulte', EntityType::class, [
+            ->add('difficulte', ChoiceType::class, [
                 'label' => 'Niveau de difficulté',
                 'attr' => ['class' => 'form-control'],
                 'placeholder' => '-- Choisir le niveau de difficulté -- ',
-                'class' => NiveauDifficulte::class,
-                'choice_label' => 'description'
+                'choices' => [
+                    'Facile'    => 1,
+                    'Moyen'     => 2,
+                    'Difficile' => 3
+                ]
             ])
-            ->add('prix', EntityType::class, [
+            ->add('prix', ChoiceType::class, [
                 'label' => 'Niveau de prix',
                 'attr' => ['class' => 'form-control'],
                 'placeholder' => '-- Choisir le niveau de prix -- ',
-                'class' => CategoriesPrix::class,
-                'choice_label' => function (CategoriesPrix $categorie) {
-                    return strtoupper($categorie->getDescription());
-                }
+                'choices' => [
+                    'Bon marché' => 1,
+                    'Moyen'      => 2,
+                    'Assez cher' => 3,
+                    'Cher'       => 4
+                ]
             ]);
 
         $form = $builder->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $recette = $form->getData();
+            $em->persist($recette);
+            $em->flush();
+        }
 
         $formView = $form->createView();
 
