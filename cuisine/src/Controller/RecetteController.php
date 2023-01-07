34c1ac77097b6e282d7d9dfca2a1054cea9fd629
François Bonnafous <francois.bonnafous@hotmail.fr>
2023-01-07 22:10:25 +0100

    Création des fonction de visualisation des recettes et affichage

--- cuisine/src/Controller/RecetteController.php
+++ cuisine/src/Controller/RecetteController.php
@@ -4,16 +4,51 @@ namespace App\Controller;
 
 use App\Entity\Recette;
 use App\Form\RecetteType;
+use App\Repository\CategoriesPrixRepository;
+use App\Repository\NiveauDifficulteRepository;
 use App\Repository\RecetteRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+/**
+ * @Route("/recette")
+ */
+
 class RecetteController extends AbstractController
 {
+
     /**
-     * @Route("/recette/{id}/edit", name="recette_edit")
+     * @Route("/", name="recette_index")
+     */
+    public function index(RecetteRepository $recetteRepository)
+    {
+        return $this->render('recette/index.html.twig', [
+            'recettes' => $recetteRepository->findBy(array(), array('nom' => 'ASC')),
+        ]);
+    }
+
+    /**
+     * @Route("/show/{id}", name="recette_show")
+     */
+    public function show($id, RecetteRepository $recetteRepository, NiveauDifficulteRepository $niveauDifficulteRepository, CategoriesPrixRepository $categoriesPrixRepository)
+    {
+        $recette = $recetteRepository->find($id);
+
+        $niveau = $niveauDifficulteRepository->findOneBy(['niveau' => $recette->getdifficulte()])->getDescription();
+
+        $prix = $categoriesPrixRepository->findOneBy(['categorie' => $recette->getprix()])->getDescription();
+
+        return $this->render('recette/show.html.twig', [
+            'recette' => $recette,
+            'niveau'  => $niveau,
+            'prix'    => $prix
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="recette_edit")
      */
     public function edit($id, RecetteRepository $recetteRepository, Request $request, EntityManagerInterface $em)
     {
@@ -30,6 +65,7 @@ class RecetteController extends AbstractController
         if ($form->isSubmitted()) {
             $em->flush();
             $this->addFlash('success', "Recette modifiée");
+            return $this->redirectToRoute('recette_index');
         }
 
         $formView = $form->createView();
@@ -41,7 +77,7 @@ class RecetteController extends AbstractController
     }
 
     /**
-     * @Route("/recette/create", name="recette_create")
+     * @Route("/create", name="recette_create")
      */
     public function create(Request $request, EntityManagerInterface $em)
     {
@@ -59,6 +95,7 @@ class RecetteController extends AbstractController
             $em->persist($recette);
             $em->flush();
             $this->addFlash('success', "Recette créée");
+            return $this->redirectToRoute('recette_index');
         }
 
         $formView = $form->createView();
